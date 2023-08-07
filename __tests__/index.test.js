e7d1ecaecdc65ca2daa5621455d2de78702cfce8
plutorbito <plutorbito@gmail.com>
2023-08-07 18:19:19 +0200

    project step 4 updates

---
+++ __tests__/index.test.js
@@ -0,0 +1,23 @@
+import genDiff from "../src/index.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('genDiff1', () => {
+   const expected = 
+`{
+ - follow: false
+   host: hexlet.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 20
+ + verbose: true
+}`;
+
+   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expected);
+});
