8efe1164f51f74b9a894c56523372ddc67a608c6
plutorbito <plutorbito@gmail.com>
2023-08-09 14:56:35 +0200

    project step 6 updates

--- src/makeAsTree.js
+++ src/makeAsTree.js
@@ -4,16 +4,36 @@ const makeAsTree = (obj1, obj2) => {
    const allKeys = _.sortBy(Object.keys({ ...obj1, ...obj2 }));
    let differenceData = [];
    allKeys.forEach((key) => {
-     if (obj1.hasOwnProperty(key) && !obj2.hasOwnProperty(key)) {
-      differenceData.push(` - ${key}: ${obj1[key]}`);
-     } else if (!obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
-      differenceData.push(` + ${key}: ${obj2[key]}`);
-     } else if (obj1[key] !== obj2[key]) {
-      differenceData.push(` - ${key}: ${obj1[key]}`);
-      differenceData.push(` + ${key}: ${obj2[key]}`);
-     } else {
-      differenceData.push(`   ${key}: ${obj1[key]}`);
-     }
+    if (!_.isObject(obj1[key]) && !_.isObject(obj2[key])) {
+      if (obj1.hasOwnProperty(key) && !obj2.hasOwnProperty(key)) {
+        differenceData.push(` - ${key}: ${obj1[key]}`);
+      } else if (!obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
+        differenceData.push(` + ${key}: ${obj2[key]}`);
+      } else if (obj1[key] !== obj2[key]) {
+        differenceData.push(` - ${key}: ${obj1[key]}`);
+        differenceData.push(` + ${key}: ${obj2[key]}`);
+      } else {
+        differenceData.push(`   ${key}: ${obj1[key]}`);
+      }
+    }
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      const nestedDiff = makeAsTree(obj1[key], obj2[key]);
+      if (nestedDiff) {
+        differenceData.push(` ${key}: ${nestedDiff}`);
+      }
+    }
+
+    if (_.isObject(obj1[key]) && !obj2.hasOwnProperty(key)) {
+      differenceData.push(
+        `- ${key}: ${JSON.stringify(obj1[key], null, 1).replace(/\"/g, "")}`
+      );
+    }
+
+    if (!obj1.hasOwnProperty(key) && _.isObject(obj2[key])) {
+      differenceData.push(
+        `+ ${key}: ${JSON.stringify(obj2[key], null, 1).replace(/\"/g, "")}`
+      );
+    }
    });
  
    const result = ["{", ...differenceData, "}"].join("\n");
