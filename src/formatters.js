6447192a42ba5beabcc1c42e0b802b5b1d5cd2dd
plutorbito <plutorbito@gmail.com>
2023-08-11 15:30:14 +0200

    project step 6 updates

--- src/formatters.js
+++ src/formatters.js
@@ -1,20 +1,27 @@
 import _ from 'lodash';
 
 const makeStylish = (values) => {
-   const space = " ";
-   const spaceCount = 1;
-   const iter = (data, depth) => {
-     if (!_.isObject(data)) return `${data}`;
-     const lines = Object.entries(data).map(([key, value]) => {
-       const preparedValue = iter(value, depth + 4);
-       const spaces = space.repeat(depth * spaceCount + 1);
-       return `${spaces}${key}: ${preparedValue}`;
-     });
-     const outSpace = space.repeat(depth * spaceCount - spaceCount);
-     const result = ["{", ...lines, `${outSpace}}`].join("\n");
-     return result;
-   };
-   return iter(values, 1);
+  const space = "  ";
+  const spaceCount = 2;
+  const iter = (data, depth) => {
+    if (!_.isObject(data)) return `${data}`;
+    const lines = Object.entries(data).map(([key, value]) => {
+      if (
+        !key.startsWith("+") &&
+        !key.startsWith("-") &&
+        !key.startsWith(" ")
+      ) {
+        key = `${space}${key}`;
+      }
+      const preparedValue = iter(value, depth + 1);
+      const spaces = space.repeat(depth * spaceCount - 1);
+      return `${spaces}${key}: ${preparedValue}`;
+    });
+    const outSpace = space.repeat(depth * spaceCount - spaceCount);
+    const result = ["{", ...lines, `${outSpace}}`].join("\n");
+    return result;
+  };
+  return iter(values, 1);
  };
 
  export default makeStylish;
--- src/makeAsTree.js
+++ src/makeAsTree.js
@@ -4,7 +4,14 @@ const makeAsTree = (obj1, obj2) => {
    const allKeys = _.sortBy(Object.keys({ ...obj1, ...obj2 }));
    let result = {};
   allKeys.forEach((key) => {
-    if (!_.isObject(obj1[key]) && !_.isObject(obj2[key])) {
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      const nestedDiff = makeAsTree(obj1[key], obj2[key]);
+      if (nestedDiff) {
+        result[`  ${key}`] = nestedDiff;
+      }
+    }
+
+    if (!_.isObject(obj1[key]) || !_.isObject(obj2[key])) {
       if (obj1.hasOwnProperty(key) && !obj2.hasOwnProperty(key)) {
         result[`- ${key}`] = obj1[key];
       } else if (!obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
@@ -15,20 +22,6 @@ const makeAsTree = (obj1, obj2) => {
       } else {
         result[`  ${key}`] = obj1[key];
       }
-    }
-    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      const nestedDiff = makeAsTree(obj1[key], obj2[key]);
-      if (nestedDiff) {
-        result[`  ${key}`] = nestedDiff;
-      }
-    }
-
-    if (_.isObject(obj1[key]) && !obj2.hasOwnProperty(key)) {
-      result[`- ${key}`] = obj1[key];
-    }
-
-    if (!obj1.hasOwnProperty(key) && _.isObject(obj2[key])) {
-      result[`+ ${key}`] = obj2[key];
     }
   });
 
