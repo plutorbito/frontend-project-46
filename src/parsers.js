b95e9a16481f0559025eb45760922182bde715d1
plutorbito <plutorbito@gmail.com>
2023-08-16 17:00:53 +0200

    parsers refactoring

--- src/parsers.js
+++ src/parsers.js
@@ -1,15 +1,12 @@
-import { readFileSync } from 'node:fs';
-import path from 'path';
 import yaml from 'js-yaml';
 
-const getParsedData = (fullFilePath) => {
-  const format = path.extname(fullFilePath);
+const getParsedData = (file, format) => {
   switch (format) {
     case '.json':
-      return JSON.parse(readFileSync(fullFilePath));
+      return JSON.parse(file);
     case '.yml':
     case '.yaml':
-      return yaml.load(readFileSync(fullFilePath));
+      return yaml.load(file);
     default:
       throw new Error(`Unknown format: '${format}'!`);
   }
