bccb33207bc0492e995fef96f57e7e0ab5b65672
plutorbito <plutorbito@gmail.com>
2023-08-16 10:40:06 +0200

    plain refactoring

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,31 +1,35 @@
 import _ from 'lodash';
 
+const getValue = (value) => {
+  if (Array.isArray(value)) {
+    return value;
+  }
+  if (_.isObject(value)) {
+    return ['[complex value]'];
+  }
+  if (_.isString(value)) {
+    return [`'${value}'`];
+  }
+  return [`${value}`];
+}
+
 const makePlain = (data, path = '') => {
-  if (Array.isArray(data)) {
     return data.flatMap((obj) => {
       switch (obj.status) {
         case 'removed':
           return [`Property '${path}${obj.key}' was removed`];
         case 'added':
-          return [`Property '${path}${obj.key}' was added with value: ${makePlain(obj.value)}`];
+          return [`Property '${path}${obj.key}' was added with value: ${getValue(obj.value)}`];
         case 'nested changes':
           return makePlain(obj.value, `${path}${obj.key}.`);
         case 'changed':
-          return [`Property '${path}${obj.key}' was updated. From ${makePlain(obj.removedValue)} to ${makePlain(obj.addedValue)}`];
+          return [`Property '${path}${obj.key}' was updated. From ${getValue(obj.removedValue)} to ${getValue(obj.addedValue)}`];
         case 'unchanged':
           return [];
         default:
           throw new Error(`Unknown status: '${obj.status}'!`);
       }
     }).join('\n');
-  }
-  if (_.isObject(data)) {
-    return ['[complex value]'];
-  }
-  if (_.isString(data)) {
-    return [`'${data}'`];
-  }
-  return [`${data}`];
 };
 
 export default makePlain;
