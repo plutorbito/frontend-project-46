3841cbc64e76474f6d6064ecd34a202d0d5a7a10
plutorbito <plutorbito@gmail.com>
2023-08-16 12:30:47 +0200

    plain formatter refactoring

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -11,25 +11,26 @@ const getValue = (value) => {
     return [`'${value}'`];
   }
   return [`${value}`];
-}
+};
 
 const makePlain = (data, path = '') => {
-    return data.flatMap((obj) => {
-      switch (obj.status) {
-        case 'removed':
-          return `Property '${path}${obj.key}' was removed`;
-        case 'added':
-          return `Property '${path}${obj.key}' was added with value: ${getValue(obj.value)}`;
-        case 'nested changes':
-          return makePlain(obj.value, `${path}${obj.key}.`);
-        case 'changed':
-          return `Property '${path}${obj.key}' was updated. From ${getValue(obj.removedValue)} to ${getValue(obj.addedValue)}`;
-        case 'unchanged':
-          return [];
-        default:
-          throw new Error(`Unknown status: '${obj.status}'!`);
-      }
-    }).join('\n');
+  const result = data.flatMap((obj) => {
+    switch (obj.status) {
+      case 'removed':
+        return `Property '${path}${obj.key}' was removed`;
+      case 'added':
+        return `Property '${path}${obj.key}' was added with value: ${getValue(obj.value)}`;
+      case 'nested changes':
+        return makePlain(obj.value, `${path}${obj.key}.`);
+      case 'changed':
+        return `Property '${path}${obj.key}' was updated. From ${getValue(obj.removedValue)} to ${getValue(obj.addedValue)}`;
+      case 'unchanged':
+        return [];
+      default:
+        throw new Error(`Unknown status: '${obj.status}'!`);
+    }
+  }).join('\n');
+  return result;
 };
 
 export default makePlain;
