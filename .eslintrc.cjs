70b86b127364085e01ebfd501acfd3faa2d8ad11
plutorbito <plutorbito@gmail.com>
2023-08-17 12:57:36 +0200

    .eslintrc updated

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2021": true,
+        "jest": true
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
