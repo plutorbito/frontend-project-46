964ab506de88e39eff396fe8377b3f26daeeb6c5
plutorbito <plutorbito@gmail.com>
2023-08-18 13:53:15 +0200

    parser refactoring

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import selectFormatter from './formatters/index.js';
 const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
   const fullFilePath1 = path.resolve(process.cwd(), filePath1);
   const fullFilePath2 = path.resolve(process.cwd(), filePath2);
-  const format1 = path.extname(fullFilePath1);
-  const format2 = path.extname(fullFilePath2);
+  const format1 = (path.extname(fullFilePath1)).substring(1);
+  const format2 = (path.extname(fullFilePath2)).substring(1);
   const file1AsObject = getParsedData(readFileSync(fullFilePath1), format1);
   const file2AsObject = getParsedData(readFileSync(fullFilePath2), format2);
   const result = selectFormatter(makeTree(file1AsObject, file2AsObject), formatName);
--- src/parsers.js
+++ src/parsers.js
@@ -2,10 +2,10 @@ import yaml from 'js-yaml';
 
 const getParsedData = (file, format) => {
   switch (format) {
-    case '.json':
+    case 'json':
       return JSON.parse(file);
-    case '.yml':
-    case '.yaml':
+    case 'yml':
+    case 'yaml':
       return yaml.load(file);
     default:
       throw new Error(`Unknown format: '${format}'!`);
